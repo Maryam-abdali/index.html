905e9173ab632f6c2dcdaffaf34b84dbeb5fb4cb
Maryam-abdali <202402200@student.polytechnic.bh>
2025-10-19 17:44:26 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("bookappoinment").onsubmit=function(){alert("Thank you for filling the form we will contact you soon.");
+return false;
+};
+
+document.getElementByNameTag("tr")[1].onclick=function(){alert("appoinments are full for this service");
+                                                        };
